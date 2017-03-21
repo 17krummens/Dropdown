98703eaabbc15fcd3d69edcc6a8124ee8b6eecb8
Kyle Rummens <17krummens@users.noreply.github.com>
2017-03-21 14:36:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 (function ($) {
 
   $.dropdown = $.dropdown || {};
-  
+
   $.dropdown.elements = {};
   $.dropdown.generateID = function() {
     var x = Math.floor((Math.random() * 100000000000000000) + 1);
-    while($.dropdown.elements.hasOwnProperty(x)) {
+    while(!$.dropdown.elements.hasOwnProperty(x)) {
       x = Math.floor((Math.random() * 100000000000000000) + 1);
       return x;
     }
@@ -15,7 +15,18 @@
     dropdown : function() {
         return this.each(function() {
           if($(this).is('select')) {
-            console.log($.dropdown.generateID());
+            var elementID = $.dropdown.generateID();
+            $.dropdown.elements[elementID] = {
+              parent : $(this),
+              options : {}
+            }
+            $(this).children('option').each(function(i) {
+              $.dropdown.elements[elementID].options[i] = {
+                value : $(this).val(),
+                text : $(this).text()
+              };
+            });
+            console.log($.dropdown);
           }
           else {
             console.warn('This element is not a SELECT input');
